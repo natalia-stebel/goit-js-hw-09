88fadd74f1501b058aad24cd7f7335a2b67f5207
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-05-15 19:48:41 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+const isDisabled = true;
+let timeCounter = 0;
+
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
+
+function changeBodyBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+buttonStop.disabled = isDisabled;
+
+function onStartClick() {
+  buttonStart.disabled = isDisabled;
+  buttonStop.disabled = !isDisabled;
+  timeCounter = setInterval(changeBodyBackgroundColor, 1000);
+}
+
+function onStopClick() {
+  buttonStart.disabled = !isDisabled;
+  buttonStop.disabled = isDisabled;
+  clearInterval(timeCounter);
+}
